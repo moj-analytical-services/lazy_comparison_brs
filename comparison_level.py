199c08534bc021cee6babbc9c25d43fb8b5e9b7e
Tom Hepworth <thomas.hepworth@justice.gov.uk>
2023-11-07 16:00:44 +0000

    remove inheritance

--- comparison_level.py
+++ comparison_level.py
@@ -1,5 +1,4 @@
 from copy import copy
-from comparison_level_creator import ComparisonLevelCreator
 
 
 level1 = {
@@ -21,8 +20,7 @@ level3 = {
     "sql_condition": "first_name_l IS NULL OR first_name_r IS NULL",
 }
 
-# Inheritance is only needed to gain access to the 'configure' method...
-class ComparisonLevel(ComparisonLevelCreator):
+class ComparisonLevel:
 
     def __init__(
         self,
@@ -59,16 +57,77 @@ class ComparisonLevel(ComparisonLevelCreator):
 
         return cls(sql_condition, label_for_charts).configure(**level_dict)
 
+    # Feels more natural to have this on here. We can also inherit and this will work as expected...
+    def configure(
+        self,
+        *,
+        m_probability: float = None,
+        u_probability: float = None,
+        tf_adjustment_column: str = None,
+        tf_adjustment_weight: float = None,
+        tf_minimum_u_value: float = None,
+        is_null_level: bool = None,
+    ) -> "ComparisonLevel":
+        """
+        Configure the comparison level with options which are common to all
+        comparison levels.  The options align to the keys in the json
+        specification of a comparison level.  These options are usually not
+        needed, but are available for advanced users.
+
+
+        Args:
+            m_probability (float, optional): The m probability for this
+                comparison level. Defaults to None, meaning it is not set.
+            u_probability (float, optional): The u probability for this
+                comparison level. Defaults to None, meaning it is not set.
+            tf_adjustment_column (str, optional): Make term frequency adjustments for
+                this comparison level using this input column. Defaults to None,
+                meaning term-frequency adjustments will not be applied for this level.
+            tf_adjustment_weight (float, optional): Make term frequency adjustments
+                for this comparison level using this weight. Defaults to None,
+                meaning term-frequency adjustments are fully-weighted if turned on.
+            tf_minimum_u_value (float, optional): When term frequency adjustments are
+                turned on, where the term frequency adjustment implies a u value below
+                this value, use this minimum value instead. Defaults to None, meaning
+                no minimum value.
+            is_null_level (bool, optional): If true, m and u values will not be
+                estimated and instead the match weight will be zero for this column.
+                Defaults to None, equivalent to False.
+
+        Returns:
+            ComparisonLevelCreator: The instance of the ComparisonLevelCreator class
+                with the updated configuration.
+        """
+        args = locals()
+        del args["self"]
+        for k, v in args.items():
+            if v is not None:
+                setattr(self, k, v)
+
+        return self
+
     def __repr__(self):
         return f"Hello, I'm a comparison level for '{self.sql_condition}'"
 
 
 # Deserialise dictionaries
-display(ComparisonLevel.with_level_dict(level1))
-display(ComparisonLevel.with_level_dict(level2))
-display(ComparisonLevel.with_level_dict(level3))
+comp_level1 = ComparisonLevel.with_level_dict(level1)
+comp_level2 = ComparisonLevel.with_level_dict(level2)
+comp_level3 = ComparisonLevel.with_level_dict(level3)
+
+print(comp_level1)
+print(comp_level2)
+print(comp_level3)
+
+# Additional args constructed from depacking dict...
+comp_level1.is_null_level
+comp_level2.m_probability
 
 # Construct directly with a ComparisonLevel and arg names...
+# Allows a more user friendly building experience...
 display(ComparisonLevel(sql_condition="my_col_l = my_col_r"))
 
-ComparisonLevel(sql_condition="my_col_l = my_col_r")
+ComparisonLevel(sql_condition="my_col_l = my_col_r").configure(
+    m_probability=0.8,
+    tf_adjustment_column="my_col"
+)
